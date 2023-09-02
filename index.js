851f359264852c740fed5591b49508727e8886d5
Sami <s.babouche@esi-sba.dz>
2023-09-02 14:45:51 +0100

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+//     1) has a function expression called divide
+//     2) divide divides 2000 by 100
+//     3) has an arrow function called square
+//     4) square arrow function takes one parameter and multiplies it times itself
+//     5) has an arrow function called add
+//     6) add arrow function takes two parameters and adds them together
+
+const divide=(numa,numb)=> numa/numb;
+const square= a => a*a;
+const add = (a,b)=> a+b;
